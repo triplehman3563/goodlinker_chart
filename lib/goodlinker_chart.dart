691f773e1f194a96307791ae70f982e9fd0ec9e1
ming.chen <ming.chen@goodlinker.io>
2022-07-20 11:18:21 +0800

    x axis zoom and scroll

--- lib/goodlinker_chart.dart
+++ lib/goodlinker_chart.dart
@@ -5,6 +5,6 @@ export 'src/TimestampXAxisBarChart.dart';
 export 'entry/ChartRuleLine.dart';
 export 'entry/TimestampXAxisData.dart';
 export 'entry/TimestampXAxisDataSet.dart';
-
-
+export 'src/widget/LineChart.dart';
+export 'src/widget/BarChart.dart';
 
